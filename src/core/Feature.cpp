1be6ea458dfc9add13ac1d767167a523a6d6de54
error23 <error23.d@gmail.com>
2021-04-02 13:09:01 +0200

    feature cpp remove features

--- src/core/Feature.cpp
+++ src/core/Feature.cpp
@@ -6,17 +6,6 @@
 #include "core/Feature.hpp"
 
 /**
- * Initialise Feature and add it to static features
- *
- * @param alwaysLoop indicates if feature should always loop
- */
-CrowOs::Core::Feature::Feature(boolean alwaysLoop)
-	: alwaysLoop(alwaysLoop) {
-
-	features.push_back(this);
-}
-
-/**
  * indicates if feature should always loop
  *
  * @return alwaysLoop
