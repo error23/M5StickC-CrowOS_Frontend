d4379142c95dedce9db4a398c5f468217badd643
error23 <error23.d@gmail.com>
2021-04-14 00:00:07 +0200

    feature initialiser

---
+++ src/feature/FeatureInitialiser.cpp
@@ -0,0 +1,8 @@
+#include "core/FeatureFactory.hpp"
+
+/**
+ * Initialise all feature factories
+ * You should implement this method in order to declare feature factories to the os
+ */
+void initialiseFeatureFactories() {
+}
